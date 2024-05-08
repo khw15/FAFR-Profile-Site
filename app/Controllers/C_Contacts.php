107ddf29f144dd29d48d11dcd2a8e05cacc7c2a7
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2024-05-09 02:28:33 +0700

    fix: preserve line breaks in message field
    Ensure line breaks are preserved in the message field when submitted by converting newline characters to <br> tags. This fix prevents line breaks from being removed when the message is stored in the database or sent via email.
    
    - Modified the message handling logic to correctly convert newline characters to <br> tags.
    - Used nl2br() function to convert newline characters in the message.
    - Added conditional check to handle cases where the message is null or an array.

--- app/Controllers/C_Contacts.php
+++ app/Controllers/C_Contacts.php
@@ -3,31 +3,50 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\M_Contacts;
 
 class C_Contacts extends BaseController
 {
     public function index()
     {
-        // Send survei data to database
-
         // Get data from form
-        $data = [
-            'name' => $this->request->getPost('name'),
-            'email' => $this->request->getPost('email'),
-            'subject' => $this->request->getPost('subject'),
-            'message' => $this->request->getPost('message')
-        ];
+        $name = $this->request->getPost('name');
+        $email = $this->request->getPost('email');
+        $subject = $this->request->getPost('subject');
+        $unformated_message = $this->request->getPost('message');
+        $message = is_array($unformated_message) || $unformated_message === null ? '' : nl2br($unformated_message); // Convert newlines to <br> tags
 
         // Insert data to database
         $model = new M_Contacts();
+        $data = [
+            'name' => $name,
+            'email' => $email,
+            'subject' => $subject,
+            'message' => $message
+        ];
         $model->insertContacts($data);
 
         // Send the survey data to the email
-        $model->sendEmail($data);
+        $this->sendEmail($name, $email, $subject, $message);
 
         // Redirect to survei page
         return redirect()->to('/');
+    }
+
+    // Send email function
+    private function sendEmail($name, $email, $subject, $message)
+    {
+        $emailService = \Config\Services::email();
+
+        // Email configuration
+        $emailService->setTo('afaisalalmunawar0315@gmail.com');
+        $emailService->setFrom('web@0315.my.id', $name);
+        $emailService->setReplyTo($email);
+        $emailService->setSubject($subject);
+        $emailService->setMessage($message);
+        $emailService->setPriority(1);
+
+        // Send email
+        $emailService->send();
     }
 }
